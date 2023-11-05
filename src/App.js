f4a59ef4148e3dcf390de4dbdd6fef0357aa90da
CallistaEve <eyaurentius@gmail.com>
2023-11-05 20:13:02 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import { HomePage } from './homepage';
-import { OtherPage } from './otherPage';
+import React, { useEffect } from 'react';
 import {
-  BrowserRouter,
+  BrowserRouter as Router,
   Link,
   Route,
   Routes,
 } from "react-router-dom";
-
-import { useEffect } from 'react';
+import { HomePage } from './homepage';
+import { OtherPage } from './otherPage';
+import { ExpertPage } from "./expertPage";
 import { login, register } from './controller/auth_controller/auth_controller';
 
 function App() {
   useEffect(() => {
     const tryLogin = async () => {
-      const data =  await login("gg@gmail.com", "Awd123");
+      const data = await login("gg@gmail.com", "Awd123");
       console.log(data);
     }
     tryLogin();
-  }, [])
-  return <HomePage/>;
+  }, []);
+
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/other">Other Page</Link>
+          </li>
+          <li>
+            <Link to="/listExpert">Expert Page</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/other" element={<OtherPage />} />
+        <Route path="/listExpert" element={<ExpertPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/expertPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import CommonCtaArea from '../../components/CommonCtaArea/CommonCtaArea';
+// import CommonPageHeader from '../../components/CommonPageHeader/CommonPageHeader';
+// import Footer from '../../components/shared/Footer';
+// import PageHelmet from '../../components/shared/PageHelmet';
+// import HomeOneHeader from '../Home/HomeOneHeader/HomeOneHeader';
+import ListExpert from './listExpertPage';
+
+export const ExpertPage = () => {
+   return (
+      <>
+         {/* <PageHelmet pageTitle="Team Page" />
+
+         <HomeOneHeader/>
+         <CommonPageHeader title="Our Team" subtitle="Team" /> */}
+         <ListExpert/>
+         {/* <CommonCtaArea/>
+         <Footer/> */}
+      </>
+   );
+};
+
+export default ExpertPage;
